7964b654f2a9c8442abcf1c71824555836f477e9
Guimarães Mahota <guimaraesmahota@gmail.com>
2021-03-09 22:11:28 +0200

    add formulario funcionarios

---
+++ src/Primavera.RH.Funcionarios/UI/FDU_FichaFuncionario.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace Primavera.RH.Funcionarios.UI
+{
+    partial class FDU_FichaFuncionario
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtSalario = new System.Windows.Forms.NumericUpDown();
+            this.btProcessar = new System.Windows.Forms.Button();
+            this.btCancelar = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.txtSalario)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btCancelar);
+            this.groupBox1.Controls.Add(this.btProcessar);
+            this.groupBox1.Controls.Add(this.tableLayoutPanel1);
+            this.groupBox1.Location = new System.Drawing.Point(13, 17);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(400, 179);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Dados Funcionarios";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 15.73333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 84.26667F));
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.txtCodigo, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.txtNome, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.txtSalario, 1, 2);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(6, 20);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(375, 89);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Codigo:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nome:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 54);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Salario:";
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Location = new System.Drawing.Point(62, 3);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(302, 21);
+            this.txtCodigo.TabIndex = 3;
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(62, 30);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(302, 21);
+            this.txtNome.TabIndex = 4;
+            // 
+            // txtSalario
+            // 
+            this.txtSalario.Location = new System.Drawing.Point(62, 57);
+            this.txtSalario.Maximum = new decimal(new int[] {
+            -727379969,
+            232,
+            0,
+            0});
+            this.txtSalario.Name = "txtSalario";
+            this.txtSalario.Size = new System.Drawing.Size(302, 21);
+            this.txtSalario.TabIndex = 5;
+            // 
+            // btProcessar
+            // 
+            this.btProcessar.Location = new System.Drawing.Point(306, 129);
+            this.btProcessar.Name = "btProcessar";
+            this.btProcessar.Size = new System.Drawing.Size(75, 23);
+            this.btProcessar.TabIndex = 1;
+            this.btProcessar.Text = "Processar";
+            this.btProcessar.UseVisualStyleBackColor = true;
+            this.btProcessar.Click += new System.EventHandler(this.btProcessar_Click);
+            // 
+            // btCancelar
+            // 
+            this.btCancelar.Location = new System.Drawing.Point(209, 129);
+            this.btCancelar.Name = "btCancelar";
+            this.btCancelar.Size = new System.Drawing.Size(75, 23);
+            this.btCancelar.TabIndex = 2;
+            this.btCancelar.Text = "Cancelar";
+            this.btCancelar.UseVisualStyleBackColor = true;
+            // 
+            // FDU_FichaFuncionario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Calibri", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "FDU_FichaFuncionario";
+            this.Size = new System.Drawing.Size(428, 212);
+            this.Text = "Ficha Funcionario";
+            this.groupBox1.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.txtSalario)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.NumericUpDown txtSalario;
+        private System.Windows.Forms.Button btCancelar;
+        private System.Windows.Forms.Button btProcessar;
+    }
+}
---
+++ src/Primavera.RH.Funcionarios/UI/FDU_FichaFuncionario.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Primavera.Extensibility.BusinessEntities;
+using Primavera.Extensibility.CustomForm;
+using RhpBE100;
+
+namespace Primavera.RH.Funcionarios.UI
+{
+    public partial class FDU_FichaFuncionario : CustomForm
+    {
+        public FDU_FichaFuncionario()
+        {
+            InitializeComponent();
+        }
+
+        private void btProcessar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string str_codigo = txtCodigo.Text;
+                string str_nome = txtNome.Text;
+                decimal dbl_salario = txtSalario.Value;
+
+                var funcionario = new RhpBEFuncionario();
+
+                if (BSO.RecursosHumanos.Funcionarios.Existe(str_codigo))
+                {
+                    //PSO.Dialogos.MostraAviso("Atenção o Funcionario Já existe !!!");
+                }
+                else
+                {
+                    funcionario.Funcionario = str_codigo;
+                    funcionario.Nome = str_nome;
+                    funcionario.VencimentoMensal = (double)dbl_salario;
+                    funcionario.Vencimento = (double)dbl_salario;
+                    funcionario.TipoRendimento = "A";
+                    funcionario.TabIRPS = "TAB2014";
+                    funcionario.DataAdmissao = DateTime.Now;
+                    funcionario.Situacao = "001";
+                    funcionario.Instrumento = "001";
+                    funcionario.Estabelecimento = "001";
+
+                    BSO.RecursosHumanos.Funcionarios.Actualiza(funcionario);
+                    PSO.Dialogos.MostraAviso("Gravado Com Sucesso");
+                }
+
+                
+            }
+            catch(Exception ex)
+            {
+                PSO.Dialogos.MostraAviso("Aconteceu um erro na gravação",
+                    StdPlatBS100.StdBSTipos.IconId.PRI_Exclama,
+                    ex.Message);
+            }
+        }
+    }
+}
